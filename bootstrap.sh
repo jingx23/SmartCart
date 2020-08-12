b883c9e4a7cf1a597df839d217bb2c15f0c398b1
Jan Scheithauer <j.scheithauer@gmail.com>
2020-08-12 23:43:04 +0200

    Add swiftlint und swiftformat as run script with mint

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+brew update
+
+function install_current {
+  echo "trying to update $1"
+  # https://docs.travis-ci.com/user/reference/osx/
+  # avoids errors
+  brew upgrade $1 || brew install $1 || true
+  brew link $1
+}
+
+if [ -e "Mintfile" ]; then
+  install_current libxml2
+  install_current mint
+  mint bootstrap
+fi
