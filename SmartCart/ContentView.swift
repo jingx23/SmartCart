1c90e3f9a4edff16c8c63e079dea49a57e7962a0
Jan Scheithauer <j.scheithauer@gmail.com>
2020-08-12 23:37:26 +0200

    Initial commit Xcode structure

---
+++ SmartCart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartCart
+//
+//  Created by Jan Scheithauer on 12.08.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmartCart/SmartCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCartApp.swift
+//  SmartCart
+//
+//  Created by Jan Scheithauer on 12.08.20.
+//
+
+import SwiftUI
+
+@main
+struct SmartCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
